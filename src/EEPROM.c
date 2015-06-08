8a374a1e140fba0556d446b7658c62a79232da22
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-06-09 00:24:19 +0200

    New PID values for altitude hold

--- src/EEPROM.c
+++ src/EEPROM.c
@@ -72,9 +72,9 @@ void setDefaultConfig(void) {
     cfg.pidYawValues.Kd = 0.00040f;
     cfg.pidYawValues.integrationLimit = 10.0f; // Prevent windup
 
-    cfg.pidAltHoldValues.Kp = 0.035f;
-    cfg.pidAltHoldValues.Ki = 0.01f;
-    cfg.pidAltHoldValues.Kd = 0.00300f;
+    cfg.pidAltHoldValues.Kp = 0.040f;
+    cfg.pidAltHoldValues.Ki = 0.03f;
+    cfg.pidAltHoldValues.Kd = 0.00350f;
     cfg.pidAltHoldValues.integrationLimit = 10.0f; // Prevent windup
 
     resetPIDRollPitchYaw();
