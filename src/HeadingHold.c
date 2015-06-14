32cdc40b063c705afa34303d92481d6e072da2a6
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-06-14 22:48:32 +0200

    Rename variable, so it becomes more clear that it is in fact just a P controller

--- src/HeadingHold.c
+++ src/HeadingHold.c
@@ -43,12 +43,12 @@ float updateHeadingHold(angle_t *angle, float rudder, uint32_t __attribute__((un
         static const uint32_t interval = 10e6; // 10 seconds between steps
         magHold = logStateMachine(getRXChannel(RX_AUX2_CHAN) > 0, magHold, angle->axis.yaw, step1, step2, interval, now);
 #endif
-        float diff = magHold - angle->axis.yaw;
-        if (diff < -180.0f) // Normalize difference, so 0 is forward and -180 and 180 is backward
-            diff += 360.0f;
-        else if (diff > 180.0f)
-            diff -= 360.0f;
-        rudder += diff * cfg.headKp;
+        float error = magHold - angle->axis.yaw;
+        if (error < -180.0f) // Normalize difference, so 0 is forward and -180 and 180 is backward
+            error += 360.0f;
+        else if (error > 180.0f)
+            error -= 360.0f;
+        rudder += error * cfg.headKp; // Add the output from the P controller to the rudder input
 #if !(STEP_ACRO_SELF_LEVEL || STEP_ALTITUDE_HOLD || STEP_HEADING_HOLD)
         GPIOPinWrite(GPIO_LED_BASE, GPIO_BLUE_LED, GPIO_BLUE_LED); // Turn on blue LED
 #endif
