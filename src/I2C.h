df4c952976172cddbddaca888fc84d72e097bdf6
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-09-07 14:39:40 +0200

    Fix typo

--- src/I2C.h
+++ src/I2C.h
@@ -24,7 +24,7 @@ extern "C" {
 
 void initI2C(void);
 void i2cWrite(uint8_t addr, uint8_t regAddr, uint8_t data);
-void i2cWriteData(uint8_t addr, uint8_t regAddr, uint8_t *date, uint8_t length);
+void i2cWriteData(uint8_t addr, uint8_t regAddr, uint8_t *data, uint8_t length);
 uint8_t i2cRead(uint8_t addr, uint8_t regAddr);
 void i2cReadData(uint8_t addr, uint8_t regAddr, uint8_t *data, uint8_t length);
 
