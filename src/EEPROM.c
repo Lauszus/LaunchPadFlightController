Lauszus/LaunchPadFlightController

commit 9f6ec76b513da1b92a2425234e9824c2bbe320df

Use NED-coordinate system

src/EEPROM.c
@@ -27,7 +27,7 @@
 #include "utils/uartstdio.h" // Add "UART_BUFFERED" to preprocessor
 #endif
 
-static const uint32_t configVersion = 17; // Must be bumped every time config_t is changed
+static const uint32_t configVersion = 18; // Must be bumped every time config_t is changed
 config_t cfg;
 
 void initEEPROM(void) {

src/MPU6500.c
@@ -57,13 +57,13 @@ bool dataReadyMPU6500(void) {
 }
 
 // X-axis should be facing forward
-// Y-axis should be facing to the left
-// Z-axis should be facing upward
+// Y-axis should be facing to the right
+// Z-axis should be facing downward
 void mpu6500BoardOrientation(sensorRaw_t *sensorRaw) {
     sensorRaw_t sensorRawTemp = *sensorRaw;
     sensorRaw->axis.X = -sensorRawTemp.axis.Y;
-    sensorRaw->axis.Y = sensorRawTemp.axis.X;
-    sensorRaw->axis.Z = sensorRawTemp.axis.Z;
+    sensorRaw->axis.Y = -sensorRawTemp.axis.X;
+    sensorRaw->axis.Z = -sensorRawTemp.axis.Z;
 }
 
 // Returns accelerometer and gyro data with zero values subtracted
@@ -71,9 +71,9 @@ void getMPU6500Data(mpu6500_t *mpu6500) {
     uint8_t buf[14];
     i2cReadData(MPU6500_ADDRESS, MPU6500_ACCEL_XOUT_H, buf, 14); // Note that we can't write directly into mpu6500_t, because of endian conflict. So it has to be done manually
 
-    mpu6500->acc.axis.X = (int16_t)((buf[0] << 8) | buf[1]);
-    mpu6500->acc.axis.Y = (int16_t)((buf[2] << 8) | buf[3]);
-    mpu6500->acc.axis.Z = (int16_t)((buf[4] << 8) | buf[5]);
+    mpu6500->acc.axis.X = -((int16_t)((buf[0] << 8) | buf[1])); // The MPU-6500/MPU-9250 assumes that it reads +1g when pointing away from gravity, so we have to invert the values
+    mpu6500->acc.axis.Y = -((int16_t)((buf[2] << 8) | buf[3]));
+    mpu6500->acc.axis.Z = -((int16_t)((buf[4] << 8) | buf[5]));
 
     mpu6500->gyro.axis.X = (int16_t)((buf[8] << 8) | buf[9]);
     mpu6500->gyro.axis.Y = (int16_t)((buf[10] << 8) | buf[11]);
@@ -87,6 +87,12 @@ void getMPU6500Data(mpu6500_t *mpu6500) {
         mpu6500->gyro.data[axis] -= gyroZero.data[axis]; // Subtract gyro zero values
         mpu6500->gyroRate.data[axis] = (float)mpu6500->gyro.data[axis] / mpu6500->gyroScaleFactor; // Convert to deg/s
     }
+/*
+    // Acceleration should be positive when aligned with the gravity vector and gyro should follow the right hand rule
+    UARTprintf("%d\t%d\t%d\t", mpu6500->acc.axis.X, mpu6500->acc.axis.Y, mpu6500->acc.axis.Z);
+    UARTprintf("%d\t%d\t%d\n", mpu6500->gyro.axis.X, mpu6500->gyro.axis.Y, mpu6500->gyro.axis.Z);
+    UARTFlushTx(false);
+*/
 }
 
 static bool checkMinMax(int32_t *array, uint8_t length, int16_t maxDifference) { // Used to check that the flight controller is not moved while calibrating
@@ -147,9 +153,8 @@ static bool calibrateMPU6500Gyro(void) {
 
 bool calibrateMPU6500Acc(mpu6500_t *mpu6500) {
     bool rcode = calibrateSensor(&cfg.accZero, MPU6500_ACCEL_XOUT_H, 100); // 100 / 4096 ~= 0.02g
-    cfg.accZero.axis.Z -= mpu6500->accScaleFactor; // Z-axis is reading +1g when horizontal, so we subtract 1g from the value found
-
     if (!rcode) {
+        cfg.accZero.axis.Z += mpu6500->accScaleFactor; // Z-axis is reading +1g when horizontal, so we add 1g to the value, as the accelerometer is inverted in the MPU-6500/MPU-9250
 #if UART_DEBUG
         UARTprintf("Accelerometer zero values: %d\t%d\t%d\n", cfg.accZero.axis.X, cfg.accZero.axis.Y, cfg.accZero.axis.Z);
 #endif

src/AK8963.c
@@ -76,8 +76,8 @@ bool initAK8963(void) {
 }
 
 // X-axis should be facing forward
-// Y-axis should be facing to the left
-// Z-axis should be facing upward
+// Y-axis should be facing to the right
+// Z-axis should be facing downward
 static void ak8963BoardOrientation(sensorRaw_t *sensorRaw) {
     // Note that the AK8963 is aligned differently compared to the MPU-9250 - see: http://store.invensense.com/datasheets/invensense/MPU9250REV1.0.pdf page 38
     // x -> y
@@ -107,7 +107,11 @@ void getAK8963Data(sensor_t *mag, bool calibrating) {
     }
 
     ak8963BoardOrientation(&magRaw); // Apply board orientation
-
+/*
+    // The value should be positive when pointing at north
+    UARTprintf("%d\t%d\t%d\n", magRaw.axis.X, magRaw.axis.Y, magRaw.axis.Z);
+    UARTFlushTx(false);
+*/
     for (uint8_t axis = 0; axis < 3; axis++) {
         mag->data[axis] = magRaw.data[axis];
         if (!calibrating) // If we are not calibrating, then subtract zero values

src/AltitudeHold.c
@@ -102,11 +102,12 @@ void getAltitude(angle_t *angle, mpu6500_t *mpu6500, altitude_t *altitude, uint3
             .axis = {
                     .roll = -angle->axis.roll * DEG_TO_RAD,
                     .pitch = -angle->axis.pitch * DEG_TO_RAD,
-                    .yaw = angle->axis.yaw * DEG_TO_RAD, // Yaw angle is already inverted
+                    .yaw = -angle->axis.yaw * DEG_TO_RAD,
             }
     };
     sensor_t accInertialFrame = mpu6500->accBodyFrame;
     rotateV(&accInertialFrame, &rotAngle);
+    float accelerationZ = -accInertialFrame.axis.Z; // Since the z-axis is pointing downward we invert it
 
     /* Estimate altitude and velocity using barometer */
     static float baro_noise_lpf = 0.95f; // TODO: Set via app
@@ -131,7 +132,7 @@ void getAltitude(angle_t *angle, mpu6500_t *mpu6500, altitude_t *altitude, uint3
     /* Estimate altitude and velocity using acceleration */
     // Fist subtract 1g datasheet value, so it is reading 0g when it is flat, then the value is actually converted into g's, then into m/s^2 and finally into cm/s^2
     static const float gravitationalAcceleration = 9.80665f; // See: https://en.wikipedia.org/wiki/Gravitational_acceleration
-    altitude->acceleration = (float)(accInertialFrame.axis.Z - mpu6500->accScaleFactor) / mpu6500->accScaleFactor * gravitationalAcceleration * 100.0f;
+    altitude->acceleration = (float)(accelerationZ - mpu6500->accScaleFactor) / mpu6500->accScaleFactor * gravitationalAcceleration * 100.0f;
 
     float accDt = altitude->acceleration * dt; // Limit number of multiplications
     float accVelocity = altitude->velocity + accDt; // Estimate velocity using acceleration

src/HMC5883L.c
@@ -101,13 +101,13 @@ static void drdyHandler(void) {
 }
 
 // X-axis should be facing forward
-// Y-axis should be facing to the left
-// Z-axis should be facing upward
+// Y-axis should be facing to the right
+// Z-axis should be facing downward
 static void hmc5883lBoardOrientation(sensor_t *sensor) {
     sensor_t sensorTemp = *sensor;
     sensor->axis.X = sensorTemp.axis.Y;
-    sensor->axis.Y = sensorTemp.axis.X;
-    sensor->axis.Z = -sensorTemp.axis.Z;
+    sensor->axis.Y = -sensorTemp.axis.X;
+    sensor->axis.Z = sensorTemp.axis.Z;
 }
 
 static void getHMC5883LDataRaw(sensorRaw_t *magRaw) {
@@ -125,7 +125,11 @@ void getHMC5883LData(sensor_t *mag, bool calibrating) {
         mag->data[axis] = (float)hmc5883l.magRaw.data[axis] * hmc5883l.magGain.data[axis]; // Apply gain
 
     hmc5883lBoardOrientation(mag); // Apply board orientation
-
+/*
+    // The value should be positive when pointing at north
+    UARTprintf("%d\t%d\t%d\n", hmc5883l.magRaw.axis.X, hmc5883l.magRaw.axis.Y, hmc5883l.magRaw.axis.Z);
+    UARTFlushTx(false);
+*/
     if (!calibrating) { // If we are not calibrating, then subtract zero values
         for (uint8_t axis = 0; axis < 3; axis++)
             mag->data[axis] -= cfg.magZero.data[axis]; // Subtract zero value stored in EEPROM

src/IMU.c
@@ -31,7 +31,7 @@ static float calculateHeading(angle_t *angle, sensor_t *mag);
 
 // Accelerometer readings can be in any scale, but gyro rate needs to be in deg/s
 // Make sure that roll increases when tilting quadcopter to the right, pitch increases
-// when pitching quadcopter downward and yaw increases when rotating quadcopter clockwise.
+// when pitching quadcopter upward and yaw increases when rotating quadcopter clockwise.
 void getAngles(mpu6500_t *mpu6500, sensor_t *mag, angle_t *angle, float dt) {
     static const float acc_lpf_factor = 4.0f;
     static const float gyro_cmpf_factor = 600.0f;
@@ -101,6 +101,7 @@ void getAngles(mpu6500_t *mpu6500, sensor_t *mag, angle_t *angle, float dt) {
     for (uint8_t axis = 0; axis < 3; axis++)
         gyroAngle.data[axis] += mpu6500->gyroRate.data[axis] * dt; // Gyro angle is only used for debugging
 
+    // Make sure the data goes in the same direction
     UARTprintf("%d\t%d\t", (int16_t)gyroAngle.axis.roll, (int16_t)angle->axis.roll);
     UARTprintf("%d\t%d\t", (int16_t)gyroAngle.axis.pitch, (int16_t)angle->axis.pitch);
     UARTprintf("%d\t%d\n", (int16_t)gyroAngle.axis.yaw, (int16_t)angle->axis.yaw);
@@ -109,7 +110,6 @@ void getAngles(mpu6500_t *mpu6500, sensor_t *mag, angle_t *angle, float dt) {
 }
 
 // See: http://www.freescale.com/files/sensors/doc/app_note/AN4248.pdf eq. 22
-// Note heading is inverted, so it increases when rotating clockwise. This is done so it works well with the RC yaw control input
 static float calculateHeading(angle_t *angle, sensor_t *mag) {
 #if USE_MAG
     #ifndef DEBUG
@@ -131,9 +131,9 @@ static float calculateHeading(angle_t *angle, sensor_t *mag) {
     float Bfy = mag->axis.Z * sinx - mag->axis.Y * cosx;
     float Bfx = mag->axis.X * cosy + mag->axis.Y * siny * sinx + mag->axis.Z * siny * cosx;
 #if USE_MAG
-    float heading = -(atan2f(Bfy, Bfx) * RAD_TO_DEG + magneticDeclination); // Return heading
+    float heading = atan2f(Bfy, Bfx) * RAD_TO_DEG + magneticDeclination; // Return heading
 #else
-    float heading = -(atan2f(Bfy, Bfx) * RAD_TO_DEG); // Return heading
+    float heading = atan2f(Bfy, Bfx) * RAD_TO_DEG; // Return heading
 #endif
 
     if (heading < 0) // Convert heading range to 0-360
@@ -142,10 +142,11 @@ static float calculateHeading(angle_t *angle, sensor_t *mag) {
 }
 
 // Rotate accelerometer coordinate axis by a delta angle from gyroscope
+// Rotates from NED to body frame using the xyz-convention
 // See: http://mathworld.wolfram.com/RotationMatrix.html,
 // http://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions#Euler_angles_.28x-y-z_extrinsic.29_.E2.86.92_Rotation_matrix and
 // https://engineering.purdue.edu/~bethel/rot2.pdf
-void rotateV(sensor_t *v, angle_t *angle) {
+void rotateV(sensor_t *v, const angle_t *angle) {
     sensor_t v_tmp = *v;
 
     float cosx = cosf(angle->axis.roll);

src/IMU.h
@@ -28,7 +28,7 @@ extern "C" {
 #endif
 
 void getAngles(mpu6500_t *mpu6500, sensor_t *mag, angle_t *angle, float dt);
-void rotateV(sensor_t *v, angle_t *angle);
+void rotateV(sensor_t *v, const angle_t *angle);
 
 #ifdef __cplusplus
 }

src/main.c
@@ -166,7 +166,7 @@ int main(void) {
             // Motors routine
             if (runMotors) {
                 float aileron = getRXChannel(RX_AILERON_CHAN);
-                float elevator = getRXChannel(RX_ELEVATOR_CHAN);
+                float elevator = -getRXChannel(RX_ELEVATOR_CHAN); // Invert so it follows the right hand rule for the NED-coordinate system
                 float rudder = getRXChannel(RX_RUDDER_CHAN);
                 //UARTprintf("%d\t%d\t%d\n", (int16_t)aileron, (int16_t)elevator, (int16_t)rudder);
 
@@ -213,7 +213,7 @@ int main(void) {
 
                 float rollOut = updatePID(&pidRoll, setPointRoll, mpu6500.gyroRate.axis.roll, dt);
                 float pitchOut = updatePID(&pidPitch, setPointPitch, mpu6500.gyroRate.axis.pitch, dt);
-                float yawOut = updatePID(&pidYaw, setPointYaw, -mpu6500.gyroRate.axis.yaw, dt); // Gyro rate is inverted, so it works well with RC yaw control input
+                float yawOut = updatePID(&pidYaw, setPointYaw, mpu6500.gyroRate.axis.yaw, dt);
 
                 float throttle = getRXChannel(RX_THROTTLE_CHAN);
 
@@ -234,10 +234,10 @@ int main(void) {
                 motors[2] += rollOut;
                 motors[3] += rollOut;
 
-                motors[0] += pitchOut;
-                motors[1] -= pitchOut;
-                motors[2] += pitchOut;
-                motors[3] -= pitchOut;
+                motors[0] -= pitchOut;
+                motors[1] += pitchOut;
+                motors[2] -= pitchOut;
+                motors[3] += pitchOut;
 
                 motors[0] -= yawOut;
                 motors[1] += yawOut;
@@ -288,10 +288,11 @@ int main(void) {
             // Auto take off and land in altitude hold mode
         // Show distance in graph as well
     // Add disarm timer
+        // Watchdog timer as well
     // All filters should depend on dt as well, so loop time does not affect them
         // And they should also be on the same form to make it consistent
     // Store angles in radians as well
     // IMU driver should have MPU-6500 and HMC5883L instances, so they did not have to be in the main loop
-    // Make yaw right hand rotation
     // Move all IMU related code into IMU driver
         // Also make generic accGyro driver
+    // Add RX calibration routine

commit 473371bd059e6b1e983a537449e2e43779f64f37

Since the accelerometer readings from the MPU-6500 are inverted, we need to invert the zero values as well
This bug was introduced in: 9f6ec76b513da1b92a2425234e9824c2bbe320df

src/EEPROM.c
@@ -27,7 +27,7 @@
 #include "utils/uartstdio.h" // Add "UART_BUFFERED" to preprocessor
 #endif
 
-static const uint32_t configVersion = 18; // Must be bumped every time config_t is changed
+static const uint32_t configVersion = 19; // Must be bumped every time config_t is changed
 config_t cfg;
 
 void initEEPROM(void) {

src/MPU6500.c
@@ -154,7 +154,11 @@ static bool calibrateMPU6500Gyro(void) {
 bool calibrateMPU6500Acc(mpu6500_t *mpu6500) {
     bool rcode = calibrateSensor(&cfg.accZero, MPU6500_ACCEL_XOUT_H, 100); // 100 / 4096 ~= 0.02g
     if (!rcode) {
-        cfg.accZero.axis.Z += mpu6500->accScaleFactor; // Z-axis is reading +1g when horizontal, so we add 1g to the value, as the accelerometer is inverted in the MPU-6500/MPU-9250
+        // The MPU-6500/MPU-9250 assumes that it reads +1g when pointing away from gravity, so we have to invert the values
+        cfg.accZero.axis.X = -cfg.accZero.axis.X;
+        cfg.accZero.axis.Y = -cfg.accZero.axis.Y;
+        cfg.accZero.axis.Z = -cfg.accZero.axis.Z;
+        cfg.accZero.axis.Z -= mpu6500->accScaleFactor; // Z-axis should be reading +1g when horizontal, so we subtract 1g from the value
 #if UART_DEBUG
         UARTprintf("Accelerometer zero values: %d\t%d\t%d\n", cfg.accZero.axis.X, cfg.accZero.axis.Y, cfg.accZero.axis.Z);
 #endif